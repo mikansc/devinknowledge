1933a3dc8731f130e03e7652a422534d33ac8d75
Michael Nascimento <michael.nsc@outlook.com>
2022-09-02 20:17:16 -0300

    Ex1 completo

---
+++ devinknowledge/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return "Oi";
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
