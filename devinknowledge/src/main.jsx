d19b5a1782c0896e41b3898ed8e9ceaabf8161da
Michael Nascimento <michael.nsc@outlook.com>
2022-09-02 20:30:36 -0300

    Exercicio 2

--- devinknowledge/src/main.jsx
+++ devinknowledge/src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "./App";
 
-const App = () => {
-  return "Oi";
-};
+import "./assets/css/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
